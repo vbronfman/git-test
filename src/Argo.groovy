471729f0210671f8b12756a25dae40caeb1b97b8
Roman Gorelik <romang@gilat.com>
2023-05-15 18:01:03 +0300

    +get

--- src/Argo.groovy
+++ src/Argo.groovy
@@ -2,20 +2,21 @@ import groovy.json.JsonSlurperClassic
 
 class Argo
 {
-    private String token
     private def ctx
     private HashMap opt
     private def debug(message){ if (this.opt.debug) this.ctx.println("DEBUG: ${message}") }
+    private String url
+    private String token
     Argo(ctx, opt){
         this.ctx = ctx
         this.opt = opt ?: [:]
         this.opt.debug = !!this.opt.debug
+        this.url = this.opt.url ?: 'https://argocd-rnd.gilat.com/'
     }
     def connect()
     {
-        def url = this.opt.url ?: 'https://argocd-rnd.gilat.com/'
         def creds = this.opt.creds ?: '8357b9f8-f851-415f-85c5-e77332d0848a'
-        this.debug("url = $url")
+        this.debug("url = ${this.url}")
         this.debug("creds id = $creds")
         if (this.token)
         {
@@ -34,7 +35,7 @@ class Argo
             def response = this.ctx.httpRequest requestBody: body,
                 consoleLogResponseBody: verbose, httpMode: 'POST',
                 ignoreSslErrors: true, responseHandle: 'NONE', wrapAsMultipart: false,
-                url: "$url/api/v1/session"
+                url: "${this.url}/api/v1/session"
             this.debug("Status: "+response.status)
             this.debug("Content: "+response.content)
             def res = (new JsonSlurperClassic()).parseText(response.content)
@@ -42,11 +43,17 @@ class Argo
             return this.token = res.token
         }
     }
-    def getApplication(name){
+    private def get(path)
+    {
+        def t = this.connect()
         def verbose = this.opt.debug
-        def response = this.ctx.httpRequest consoleLogResponseBody: verbose,
+        return response = this.ctx.httpRequest consoleLogResponseBody: verbose,
             httpMode: 'GET', ignoreSslErrors: true, responseHandle: 'NONE',
-            wrapAsMultipart: false, url: "$url/api/v1/applications/${name}"
+            wrapAsMultipart: false, url: "$this.url/${path}",
+            customHeaders: [[maskValue: false, name: 'Authorization: ', value: "Bearer $t"], 
+    }
+    def getApplication(name){
+        def response = this.get("api/v1/applications/${name}")
         return (new JsonSlurperClassic()).parseText(response.content)
     }
 }
