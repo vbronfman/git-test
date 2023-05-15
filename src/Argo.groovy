c1fcf5e634d5f8da257e1e3899c7bb3fc1fc8d9c
Roman Gorelik <romang@gilat.com>
2023-05-15 14:41:35 +0300

    nd moer debug

--- src/Argo.groovy
+++ src/Argo.groovy
@@ -8,6 +8,8 @@ class Argo
         def url = opt.url ?: 'https://argocd-rnd.gilat.com/'
         if (opt.debug)
             println("DEBUG: url = $url")
+        else
+            println("Not a debug mode. DEBUG: url = $url")
         def creds = opt.creds ?: '8357b9f8-f851-415f-85c5-e77332d0848a'
         if (this.connections[url])
             return this.connections[url]
