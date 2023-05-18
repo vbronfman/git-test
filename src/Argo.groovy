4c8507366c9824641395cf42d26c83337addf285
Roman Gorelik <romang@gilat.com>
2023-05-18 15:03:47 +0300

    use saved auth token instead of password

--- src/Argo.groovy
+++ src/Argo.groovy
@@ -14,69 +14,38 @@ class Argo
         this.opt.debug = !!this.opt.debug
         this.url = this.opt.url ?: 'https://argocd-rnd.gilat.com/'
     }
-    def connect()
-    {
-        def creds = this.opt.creds ?: '8357b9f8-f851-415f-85c5-e77332d0848a'
-        this.debug("url = ${this.url}")
-        this.debug("creds id = $creds")
-        if (this.token)
-        {
-            this.debug("connect: using cached token")
-            return this.token
-        }
-        this.ctx.withCredentials([this.ctx.usernamePassword(
-            credentialsId: '8357b9f8-f851-415f-85c5-e77332d0848a',
-            passwordVariable: 'password',
-            usernameVariable: 'username',
-        )]) {
-            def body = """
-                {"username":"${this.ctx.username}", "password":"${this.ctx.password}"}
-            """
-            def verbose = this.opt.debug
-            def response = this.ctx.httpRequest requestBody: body,
-                consoleLogResponseBody: verbose, httpMode: 'POST',
-                ignoreSslErrors: true, responseHandle: 'NONE', wrapAsMultipart: false,
-                url: "${this.url}/api/v1/session"
-            this.debug("Status: "+response.status)
-            this.debug("Content: "+response.content)
-            def res = (new JsonSlurperClassic()).parseText(response.content)
-            this.debug("token: "+res.token)
-            return this.token = res.token
-        }
-    }
     private def req(path, mode, body)
     {
-        def t = this.connect()
         def verbose = this.opt.debug
-        def response = this.ctx.httpRequest consoleLogResponseBody: verbose,
-            httpMode: mode, ignoreSslErrors: true, responseHandle: 'NONE',
-            wrapAsMultipart: false, url: "${this.url}/${path}", requestBody: body,
-            customHeaders: [[maskValue: false, name: 'Authorization', value: "Bearer $t"]]
-        return response.content
+        this.ctx.withCredentials([this.ctx.string(
+            credentialsId: 'e5c5b02b-13e2-4fe3-85c3-09d780b9e3cd',
+            variable: 'token'
+        )]) {
+            def bearer = "Bearer " + this.ctx.token
+            def i = 0
+            this.ctx.retry(10){
+                this.debug("Fetching $path, try #${i++}")
+                def response = this.ctx.httpRequest(consoleLogResponseBody: verbose,
+                    httpMode: mode, ignoreSslErrors: true, responseHandle: 'NONE',
+                    wrapAsMultipart: false, url: "${this.url}/${path}", requestBody: body,
+                    customHeaders: [[maskValue: false, name: 'Authorization', value: bearer]])
+                return (new JsonSlurperClassic()).parseText(response.content)
+            }
+        }
     }
-    private def get(path)
-    {
-        return this.req(path, 'GET', '')
-    }
-    private def post(path, body)
-    {
-        return this.req(path, 'POST', body)
-    }
-    private def put(path, body)
-    {
-        return this.req(path, 'PUT', body)
-    }
+    private def get(path) { return this.req(path, 'GET', '') }
+    private def post(path, body) { return this.req(path, 'POST', body) }
+    private def put(path, body) { return this.req(path, 'PUT', body) }
     def getApplication(name)
     {
-        def content = this.get("api/v1/applications/${name}")
-        return (new JsonSlurperClassic()).parseText(content)
+        return this.get("api/v1/applications/${name}")
     }
     def syncApplication(name, obj)
     {
-        def res = this.post("api/v1/applications/${name}/sync", (new JsonBuilder(obj).toString()))
+        return this.post("api/v1/applications/${name}/sync", (new JsonBuilder(obj).toString()))
     }
     def updateApplication(name, obj)
     {
-        def res = this.put("api/v1/applications/${name}/spec", (new JsonBuilder(obj).toString()))
+        return this.put("api/v1/applications/${name}/spec", (new JsonBuilder(obj).toString()))
     }
 }
