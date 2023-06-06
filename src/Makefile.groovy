9e53e675f26bc40d638aa15123466c3ee5159414
Michael Sharay (EXT) <michaelre@gilat.com>
2023-06-06 17:32:28 +0300

    More line ending fixes.
    Signed-off-by: Michael Sharay (EXT) <michaelre@gilat.com>

--- src/Makefile.groovy
+++ src/Makefile.groovy
@@ -19,7 +19,7 @@ class Makefile
     }
 
     def packit(branch, sharepoint) {
-        def dbAddr = postgresql://g-versions-db.gilat.com:5432/se.4-versions-production
+        def dbAddr = 'postgresql://g-versions-db.gilat.com:5432/se.4-versions-production'
 
         ctx.withCredentials([usernamePassword(credentialsId: 'postgres-user-for-production', usernameVariable: 'dbUser', passwordVariable: 'dbPass')]) {
             ctx.sh """make packit VERSION_DB='${dbAddr}?user=$dbUser&password=$dbPass' BRANCH='${branch}' SHAREPOINT=${sharepoint ? 'sharepoint' : 'nosharepoint'}sharepoint"""
