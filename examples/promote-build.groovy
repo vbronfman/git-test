e03cd5d84a1661c52b4c7d373a7222deeb227072
Roman Gorelik <romang@gilat.com>
2023-06-27 15:47:38 +0300

    rm redundant file

--- examples/promote-build.groovy
+++
@@ -1,9 +0,0 @@
-@Library("devops@artifactory") _
-
-a = jfrog("AWS")
-def builds = a.getAllBuilds()
-echo "$builds"
-a.promoteBuild("/DevOps :: packMultiImage", 10, "IPHO",
-    "seiv-vsat-mi-pack-dev", "test", "testing promotion",
-    [:], env.BUILD_USER, true)
-
