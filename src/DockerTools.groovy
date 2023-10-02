0e720272c62b32ccb32299dc7ef87386705238cc
liranz <liranz@gilat.com>
2023-10-02 14:16:01 +0300

    Added docker tools

--- src/DockerTools.groovy
+++ src/DockerTools.groovy
@@ -7,13 +7,13 @@ class DockerTools implements Serializable {
 
     def build(name) {
         def date = steps.sh(script: 'date +%Y.%m.%d', returnStdout: true).trim()
-        env.VERSION = "${date}.${currentBuild.number}"
-        env.ARTIFACTORY_URL = ${Utilities.getConstant('artifactoryPackitURL')}
-        currentBuild.displayName = "#${env.VERSION}"
-        steps.sh "docker build --no-cache --force-rm -t ${Utilities.getConstant('artifactoryPackitURL')}/${name}:${env.VERSION} ."
+        steps.env.VERSION = "${date}.${currentBuild.number}"
+        steps.env.ARTIFACTORY_URL = ${Utilities.getConstant('artifactoryPackitURL')}
+        currentBuild.displayName = "#${steps.env.VERSION}"
+        steps.sh "docker build --no-cache --force-rm -t ${Utilities.getConstant('artifactoryPackitURL')}/${name}:${steps.env.VERSION} ."
     }
 
     def publish(name) {
-        steps.sh """docker push ${Utilities.getConstant('artifactoryPackitURL')}/${name}:${env.VERSION} """
+        steps.sh """docker push ${Utilities.getConstant('artifactoryPackitURL')}/${name}:${steps.env.VERSION} """
     }
 }
