2d26fa957d0c4577f72994987ad019023c267ea7
Michael Sharay (EXT) <michaelre@gilat.com>
2023-11-07 16:34:20 +0200

    Disallow use of 'make type: <>', use only 'make cmd: <>' which is less ambiguous.
    Signed-off-by: Michael Sharay (EXT) <michaelre@gilat.com>

--- vars/make.groovy
+++ vars/make.groovy
@@ -1,9 +1,5 @@
 def call(Map config) {
     def mk = new Makefile(this)
 
-    if (config.cmd) {
-        mk."${config.cmd}"(config)
-    } else {
-        mk."${config.type}"(config)
-    }
+    mk."${config.cmd}"(config)
 }
