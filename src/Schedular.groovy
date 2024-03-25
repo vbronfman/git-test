590c17a9122a49618f75ec5df64bdacbc14f1abc
liranz <liranz@gilat.com>
2024-03-25 16:16:33 +0200

    Added schedular

--- src/Schedular.groovy
+++ src/Schedular.groovy
@@ -3,16 +3,16 @@ class Schedular implements Serializable {
 
     Schedular(jobName) {
         this.jobName = jobName
-        this.jobName = "Developers/ipm-build/releases/r300.0"
     }
 
     static def getBuildResults(name)
     {
-        echo "Getting details on job ${jobName} releases/r300.0"
+        // this.jobName = "Developers/ipm-build/releases/r300.0"
+        echo "Getting details on job $jobName"
         def gitName = "GilatDevOps/SE4/ipm"
         def jenkins = Jenkins.getInstance()
-        def job = jenkins.getItem(jobName)
-        def results = build propagate: false, job: jobName
+        def job = jenkins.getItem($jobName)
+        def results = build propagate: false, job: $jobName
         def buildResult = results.getResult()
         def jobUrl = results.getAbsoluteUrl()
         def buildVar = results.getBuildVariables()
