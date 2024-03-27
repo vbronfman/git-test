ea699c5b06cb6c1c906145f069484fca5f49c725
liranz <liranz@gilat.com>
2024-03-27 15:47:04 +0200

    Added small fixes

--- src/Schedule.groovy
+++ src/Schedule.groovy
@@ -1,21 +1,24 @@
 class Schedule implements Serializable {
     private def jobName
 
-    // Schedule(jobName) {
-    //     this.jobName = jobName
-    // }
+    Schedule(jobName) {
+        this.jobName = jobName
+    }
 
     static def maybeBuild()
     {
-        // def gitName = "GilatDevOps/SE4/ipm"
-        def buildResult = getLastBuild('Developers/ipm-build/releases/r300.0')
+        def gitName = "GilatDevOps/SE4/ipm"
+        def buildResult = getLastBuild()
+        if (!buildResult) {
+            jobBuild()
+        }
     }
 
-    def getLastBuild(jobName)
+    def getLastBuild()
     {
         echo "Getting details on job $jobName"
         def jenkins = Jenkins.getInstance()
-        def job = jenkins.getItem($jobName)
+        def job = jenkins.getItem(jobName)
         def buildResult = job.getLastSuccessfulBuild()
         def jobUrl = results.getAbsoluteUrl()
         def buildVar = results.getBuildVariables()
@@ -27,7 +30,7 @@ class Schedule implements Serializable {
     {
         echo "Building job $jobName"
         def jenkins = Jenkins.getInstance()
-        def job = jenkins.getItem($jobName)
+        def job = jenkins.getItem(jobName)
         def results = build propagate: false, job: $jobName
         def buildResult = results.getResult()
         def jobUrl = results.getAbsoluteUrl()
